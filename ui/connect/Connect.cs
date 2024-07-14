d8cff3a767d983781da6517b51f654bec9b47dfc
Gregory Pierce <gpierce@blockimperium.xyz>
2024-07-13 23:16:35 -0400

    Update to latest migration

--- ui/connect/Connect.cs
+++ ui/connect/Connect.cs
@@ -14,6 +14,12 @@ public partial class Connect : Node2D
 	
 	[Export]
 	private PackedScene successScene;
+
+	[Export]
+	private LineEdit emailEntry;
+	
+	[Export]
+	private LineEdit otpEntry;
 	
 	[Signal]
 	public delegate void LoginSuccessfulEventHandler( string smartWalletAddress );
@@ -22,27 +28,44 @@ public partial class Connect : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		BlockchainManager.Instance.EmitLog("Ready");
+		BlockchainManager.Instance.EmitLog("Connect Ready");
 
 		actionButton.Text = "CONNECT";
 		instructionsLabel.Text = "Enter your email address. \nAn account and wallet will be created for you.";
 	
 		BlockchainClientNode.Instance.AwaitingOTP += SetStateAwaitingOTP;
-		BlockchainClientNode.Instance.SmartWalletCreated += SmartWalletCreated;
-		
-		LoginSuccessful += OnLoginSuccessful;	
+		//BlockchainClientNode.Instance.InAppWalletCreated += SetStateInAppWalletCreated;
+		//BlockchainClientNode.Instance.SmartWalletCreated += SmartWalletCreated;
+
+		//LoginSuccessful += OnLoginSuccessful;	
 	}	
 	
 	private void SetStateAwaitingOTP()
 	{		
 		isAwaitingOTP = true;
+		BlockchainManager.Instance.EmitLog("Awaiting OTP");
+
 		
 		actionButton.Text = "SUBMIT OTP";
 		instructionsLabel.Text = "A One Time Password (OTP) has been sent to this email address. Enter it here.";
+
+		otpEntry.Visible = true;
+		emailEntry.Visible = false;
+
+	}
+
+	private void SetStateInAppWalletCreated( string address )
+	{
+		BlockchainManager.Instance.EmitLog("InAppWalletAddress " + address);
+		
+		otpEntry.Visible = false;
+		emailEntry.Visible = false;
 	}
 	
 	private void SmartWalletCreated( string address )
 	{
+		BlockchainManager.Instance.EmitLog("SmartWalletCreated " + address);
+
 		instructionsLabel.Text = "Connected to wallet " + address;	
 		actionButton.Visible = false;
 		
@@ -53,17 +76,12 @@ public partial class Connect : Node2D
 	{		
 		if (isAwaitingOTP == false)
 		{
-			BlockchainClientNode.Instance.OnStartLogin();
+			BlockchainClientNode.Instance.OnStartLogin( emailEntry.Text );
 		}
 		else
 		{
-			BlockchainClientNode.Instance.OnOTPSubmit();
+			BlockchainClientNode.Instance.OnOTPSubmit( otpEntry.Text );
 		}	
-	}
-	
-	private void OnOTPSubmitPressed()
-	{
-		BlockchainManager.Instance.EmitLog("Submitting OTP");
 	}
 	
 	private void OnLoginSuccessful( string smartWalletAddress )
